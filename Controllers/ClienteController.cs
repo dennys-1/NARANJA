428ac28004a55200eb8ca3dec1c78c4ff95a5438
Renato Velazco <renato_velazco@usmp.pe>
2021-05-07 10:22:13 -0500

    vistacontroller

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace NARANJA.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private readonly ILogger<ClienteController> _logger;
+
+        public ClienteController(ILogger<ClienteController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult PagoCliente()
+        {
+            return View();
+        }
+         public IActionResult BoletaCliente()
+        {
+            return View();
+        }
+       
+    }
+}
