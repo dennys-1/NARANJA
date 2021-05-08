f82dd2ab35ffa044b6f787c2d90db76372d808a8
FRANK MARCO <FLOREZ_FRANK@USMP.PE>
2021-05-07 22:25:52 -0500

    se añadio carta en el Models

---
+++ Models/Carta.cs
@@ -0,0 +1,12 @@
+namespace NARANJA.Models
+{
+    public class Carta
+    {
+        public string id { get; set; }
+
+        public string precio { get; set; }
+
+        public string caracteristica { get; set; }
+
+    }
+}
