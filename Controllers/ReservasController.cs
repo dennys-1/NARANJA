a87e0a6b30aedd0ea18b55fd110f68291804d05b
dennys <DENNYS_ORTIZ@USMP.PE>
2021-05-22 02:08:03 -0500

    se agrego nuevo comando para exportar en excel

--- Controllers/ReservasController.cs
+++ Controllers/ReservasController.cs
@@ -4,10 +4,13 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 using NARANJA.Data;
 using NARANJA.Models;
+using OfficeOpenXml;
+using OfficeOpenXml.Table;
 
 namespace NARANJA.Controllers
 {
@@ -46,6 +49,27 @@ namespace NARANJA.Controllers
             return View();
         }
 
-        
+        public IActionResult ExportarExcel()
+{
+    string excelContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+    var reservas = _context.DataReservas.AsNoTracking().ToList();
+    using (var libro = new ExcelPackage())
+    {
+        var worksheet = libro.Workbook.Worksheets.Add("Reservas");
+        worksheet.Cells["A1"].LoadFromCollection(reservas, PrintHeaders: true);
+        for (var col = 1; col < reservas.Count + 1; col++)
+        {
+            worksheet.Column(col).AutoFit();
+        }
+
+        // Agregar formato de tabla
+        var tabla = worksheet.Tables.Add(new ExcelAddressBase(fromRow: 1, fromCol: 1, toRow: reservas.Count + 1, toColumn: 5), "Reservas");
+        tabla.ShowHeader = true;
+        tabla.TableStyle = TableStyles.Light6;
+        tabla.ShowTotal = true;
+
+        return File(libro.GetAsByteArray(), excelContentType, "Reservas.xlsx");
+    }
+}
     }
 }
